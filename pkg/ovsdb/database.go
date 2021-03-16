2fa83c38bb887597273208a3895d29d639bf7ed8
Alexey Roytman <roytman@il.ibm.com>
2021-03-17 00:23:36 +0200

    Add configurable etcd keys prefix
    Signed-off-by: Alexey Roytman <roytman@il.ibm.com>

--- pkg/ovsdb/database.go
+++ pkg/ovsdb/database.go
@@ -37,9 +37,10 @@ type DatabaseEtcd struct {
 	// map from prefix to monitors
 	monitors map[string]monitor
 	mu       sync.Mutex
+	prefix   string
 }
 
-func NewDatabaseEtcd(endpoints []string) (Databaser, error) {
+func NewDatabaseEtcd(endpoints []string, prefix string) (Databaser, error) {
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   endpoints,
 		DialTimeout: 5 * time.Second,
@@ -49,8 +50,11 @@ func NewDatabaseEtcd(endpoints []string) (Databaser, error) {
 		return nil, err
 	}
 	klog.Info("etcd client is connected")
+	if !strings.HasSuffix(prefix, "/") {
+		prefix = prefix + "/"
+	}
 	return &DatabaseEtcd{cli: cli,
-		Schemas: make(map[string]string)}, nil
+		Schemas: make(map[string]string), prefix: prefix}, nil
 }
 
 func (con *DatabaseEtcd) Close() {
@@ -63,7 +67,7 @@ func (con *DatabaseEtcd) Lock(ctx context.Context, id string) (bool, error) {
 	if err != nil {
 		return false, err
 	}
-	mutex := concurrency.NewMutex(session, "locks/"+id)
+	mutex := concurrency.NewMutex(session, con.prefix+"locks/"+id)
 	err = mutex.TryLock(cnx)
 	unlock := func() {
 		server := jrpc2.ServerFromContext(ctx)
@@ -86,6 +90,7 @@ func (con *DatabaseEtcd) Lock(ctx context.Context, id string) (bool, error) {
 			// Send notification
 			klog.V(5).Infoln("Locked")
 			go unlock()
+			// TODO move from database
 			if err := jrpc2.PushNotify(ctx, "locked", []string{id}); err != nil {
 				klog.Errorf("notification %v\n", err)
 				return
@@ -104,7 +109,7 @@ func (con *DatabaseEtcd) Unlock(ctx context.Context, id string) error {
 	if err != nil {
 		return err
 	}
-	mutex := concurrency.NewMutex(session, "locks/"+id)
+	mutex := concurrency.NewMutex(session, con.prefix+"locks/"+id)
 	// TODO
 	klog.V(5).Infof("is owner %+v\n", mutex.IsOwner())
 	mutex.Unlock(ctx)
@@ -122,14 +127,14 @@ func (con *DatabaseEtcd) AddSchema(schemaName, schemaFile string) error {
 	return nil
 }
 
-func (con *DatabaseEtcd) GetData(prefix string, keysOnly bool) (*clientv3.GetResponse, error) {
+func (con *DatabaseEtcd) GetData(keysPrefix string, keysOnly bool) (*clientv3.GetResponse, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
 	var resp *clientv3.GetResponse
 	var err error
 	if keysOnly {
-		resp, err = con.cli.Get(ctx, prefix, clientv3.WithPrefix(), clientv3.WithKeysOnly())
+		resp, err = con.cli.Get(ctx, con.prefix+keysPrefix, clientv3.WithPrefix(), clientv3.WithKeysOnly())
 	} else {
-		resp, err = con.cli.Get(ctx, prefix, clientv3.WithPrefix())
+		resp, err = con.cli.Get(ctx, con.prefix+keysPrefix, clientv3.WithPrefix())
 	}
 	cancel()
 	if err != nil {
@@ -147,9 +152,9 @@ func (con *DatabaseEtcd) GetData(prefix string, keysOnly bool) (*clientv3.GetRes
 }
 
 // TODO replace
-func (con *DatabaseEtcd) GetMarshaled(prefix string, columns []interface{}) (*[]map[string]string, error) {
+func (con *DatabaseEtcd) GetMarshaled(keysPrefix string, columns []interface{}) (*[]map[string]string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
-	resp, err := con.cli.Get(ctx, prefix, clientv3.WithFromKey())
+	resp, err := con.cli.Get(ctx, con.prefix+keysPrefix, clientv3.WithFromKey())
 	cancel()
 	if err != nil {
 		return nil, err
@@ -190,30 +195,30 @@ func (con *DatabaseEtcd) PutData(ctx context.Context, key string, obj interface{
 	if err != nil {
 		return err
 	}
-	_, err = con.cli.Put(ctx, key, string(data))
+	_, err = con.cli.Put(ctx, con.prefix+key, string(data))
 	if err != nil {
 		return err
 	}
 	return nil
 }
-func (con *DatabaseEtcd) AddMonitor(prefix string, mcr ovsjson.MonitorCondRequest, isV1 bool, hand *handlerKey) {
+func (con *DatabaseEtcd) AddMonitor(keysPrefix string, mcr ovsjson.MonitorCondRequest, isV1 bool, hand *handlerKey) {
 	con.mu.Lock()
 	defer con.mu.Unlock()
-	if monitor, ok := con.monitors[prefix]; !ok {
-		con.monitors[prefix] = *newMonitor(con.cli, prefix, mcr, isV1, hand)
+	if monitor, ok := con.monitors[keysPrefix]; !ok {
+		con.monitors[keysPrefix] = *newMonitor(con.cli, con.prefix+keysPrefix, mcr, isV1, hand)
 	} else {
 		monitor.addHandler(mcr, isV1, hand)
 	}
 }
 
-func (con *DatabaseEtcd) DelMonitor(prefix string, mcr ovsjson.MonitorCondRequest, isV1 bool, hand *handlerKey) {
+func (con *DatabaseEtcd) DelMonitor(keysPrefix string, mcr ovsjson.MonitorCondRequest, isV1 bool, hand *handlerKey) {
 	con.mu.Lock()
 	defer con.mu.Unlock()
-	if monitor, ok := con.monitors[prefix]; !ok {
-		klog.Warningf("Delete unexisting monitor from %s", prefix)
+	if monitor, ok := con.monitors[keysPrefix]; !ok {
+		klog.Warningf("Delete unexisting monitor from %s", keysPrefix)
 	} else {
 		if monitor.delHandler(mcr, isV1, hand) {
-			delete(con.monitors, prefix)
+			delete(con.monitors, keysPrefix)
 		}
 	}
 }
@@ -243,7 +248,7 @@ func (con *DatabaseMock) AddSchema(schemaName, schemaFile string) error {
 	return con.Error
 }
 
-func (con *DatabaseMock) GetData(prefix string, keysOnly bool) (*clientv3.GetResponse, error) {
+func (con *DatabaseMock) GetData(keysPrefix string, keysOnly bool) (*clientv3.GetResponse, error) {
 	return con.Response.(*clientv3.GetResponse), con.Error
 }
 
@@ -251,7 +256,7 @@ func (con *DatabaseMock) PutData(ctx context.Context, key string, obj interface{
 	return con.Error
 }
 
-func (con *DatabaseMock) GetMarshaled(prefix string, columns []interface{}) (*[]map[string]string, error) {
+func (con *DatabaseMock) GetMarshaled(keysPrefix string, columns []interface{}) (*[]map[string]string, error) {
 	return con.Response.(*[]map[string]string), con.Error
 }
 
@@ -263,8 +268,8 @@ func (con *DatabaseMock) GetUUID() string {
 	return con.Response.(string)
 }
 
-func (con *DatabaseMock) AddMonitor(prefix string, mcr ovsjson.MonitorCondRequest, isV1 bool, hand *handlerKey) {
+func (con *DatabaseMock) AddMonitor(keysPrefix string, mcr ovsjson.MonitorCondRequest, isV1 bool, hand *handlerKey) {
 }
 
-func (con *DatabaseMock) DelMonitor(prefix string, mcr ovsjson.MonitorCondRequest, isV1 bool, hand *handlerKey) {
+func (con *DatabaseMock) DelMonitor(keysPrefix string, mcr ovsjson.MonitorCondRequest, isV1 bool, hand *handlerKey) {
 }
