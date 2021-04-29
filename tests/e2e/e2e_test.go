3c590bd56d3dc4ef13e5ccdac988ca86e5f46cfb
OmerBenHayun <41959855+OmerBenHayun@users.noreply.github.com>
2021-04-29 10:07:12 +0300

    adding parameters to server and updating e2e (#65)
    Co-authored-by: Omer Ben-Hayun <Omer.Ben-Hayun@ibm.com>

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -3,18 +3,15 @@ package e2e_test
 import (
 	"context"
 	"encoding/json"
-	"flag"
 	"net"
 
 	"github.com/creachadair/jrpc2"
 	"github.com/creachadair/jrpc2/channel"
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
 	"k8s.io/klog/v2"
 )
-
-var serverAddr = flag.String("server", "127.0.0.1:12345", "Server address")
-var cli *jrpc2.Client
-var ctx context.Context
 
 func listDbs(ctx context.Context, cli *jrpc2.Client) (result []string, err error) {
 	err = cli.CallResult(ctx, "list_dbs", nil, &result)
@@ -40,10 +37,10 @@ func unlock(ctx context.Context, cli *jrpc2.Client, id string) (result interface
 	return
 }
 
-var _ = BeforeSuite(func() {
-	conn, err := net.Dial(jrpc2.Network(*serverAddr), *serverAddr)
+func dial(serverAddr string) (cli *jrpc2.Client, ctx context.Context) {
+	conn, err := net.Dial(jrpc2.Network(serverAddr), serverAddr)
 	if err != nil {
-		klog.Fatalf("Dial %q: %v", *serverAddr, err)
+		klog.Fatalf("Dial %q: %v", serverAddr, err)
 	}
 	klog.Infof("Connected to %v", conn.RemoteAddr())
 	// Start up the client, and enable logging to stderr.
@@ -56,53 +53,48 @@ var _ = BeforeSuite(func() {
 		AllowV1: true,
 	})
 	ctx = context.Background()
-})
+	return
+}
 
-var _ = AfterSuite(func() {
-	cli.Close()
-})
-
-var _ = Describe("E2e", func() {
-	Describe("basic db Checks", func() {
-		It("should be able to show the dbs list", func() {
-			if dbs, err := listDbs(ctx, cli); err != nil {
-				klog.Fatalln("listDbs Error:", err)
-			} else {
-				klog.Infof("listDbs result=%v", dbs)
-			}
-			return
-		})
-		It("should be able to echo a messeges", func() {
-			if echo, err := echo(ctx, cli); err != nil {
-				klog.Fatalln("echo:", err)
-			} else {
-				klog.Infof("echo result=%v", echo)
-			}
-			return
-		})
-		It("should be able to retrive the server ID", func() {
-			if uuid, err := getServerId(ctx, cli); err != nil {
-				klog.Fatalln("getServerId:", err)
-			} else {
-				klog.Infof("getServerId result=%v", uuid)
-			}
-			return
-		})
-		It("should be able to lock", func() {
-			if lock, err := lock(ctx, cli, "test1"); err != nil {
-				klog.Fatalln("lock:", err)
-			} else {
-				klog.Infof("lock result=%v", lock)
-			}
-			return
-		})
-		It("should be able to unlock", func() {
-			if lock, err := unlock(ctx, cli, "test1"); err != nil {
-				klog.Fatalf("unlock: %v", err)
-			} else {
-				klog.Infof("unlock result=%v", lock)
-			}
-			return
-		})
+var _ = Describe("e2e", func() {
+	var _ = Describe("e2e ovsdb-etcd", func() {
+		DescribeTable("ovsdb-etcd operations",
+			func(serverAddr string) {
+				cli, ctx := dial(serverAddr)
+				Context("should be able to show the dbs list", func() {
+					dbs, err := listDbs(ctx, cli)
+					Expect(err).ShouldNot(HaveOccurred())
+					klog.Infof("listDbs result=%v", dbs)
+				})
+				Context("should be able to echo a messeges", func() {
+					echo, err := echo(ctx, cli)
+					Expect(err).ShouldNot(HaveOccurred())
+					klog.Infof("echo result=%v", echo)
+				})
+				Context("should be able to echo a messeges", func() {
+					echo, err := echo(ctx, cli)
+					Expect(err).ShouldNot(HaveOccurred())
+					klog.Infof("echo result=%v", echo)
+				})
+				Context("should be able to retrive the server ID", func() {
+					uuid, err := getServerId(ctx, cli)
+					Expect(err).ShouldNot(HaveOccurred())
+					klog.Infof("getServerId result=%v", uuid)
+				})
+				Context("should be able to lock", func() {
+					lock, err := lock(ctx, cli, "test1")
+					Expect(err).ShouldNot(HaveOccurred())
+					klog.Infof("lock result=%v", lock)
+				})
+				Context("should be able to unlock", func() {
+					lock, err := unlock(ctx, cli, "test1")
+					Expect(err).ShouldNot(HaveOccurred())
+					klog.Infof("unlock result=%v", lock)
+				})
+				cli.Close()
+			},
+			Entry("ovn North-bound", "127.0.0.1:6641"),
+			//Entry("ovn South-bound", "127.0.0.1:6642"),
+		)
 	})
 })
