da9e8d6c99830e5db631cdc4e0a0b94aee4665e2
Alexey Roytman <roytman@il.ibm.com>
2021-08-13 10:23:24 +0300

    fix mutations for set scalar operations (int, real)

--- pkg/ovsdb/mutation.go
+++ pkg/ovsdb/mutation.go
@@ -20,11 +20,11 @@ const (
 )
 
 type Mutation struct {
-	Column       string
-	Mutator      string
-	Value        interface{}
-	ColumnSchema *libovsdb.ColumnSchema
-	Log          logr.Logger
+	column       string
+	mutator      string
+	value        interface{}
+	columnSchema *libovsdb.ColumnSchema
+	log          logr.Logger
 }
 
 func NewMutation(tableSchema *libovsdb.TableSchema, mapUUID namedUUIDResolver, mutation []interface{}, log logr.Logger) (*Mutation, error) {
@@ -57,13 +57,18 @@ func NewMutation(tableSchema *libovsdb.TableSchema, mapUUID namedUUIDResolver, m
 	}
 
 	value := mutation[2]
-	value, err = columnSchema.Unmarshal(value)
-	if err != nil {
-		// value for mutate map with delete mutator can be map or set
-		if columnSchema.Type == libovsdb.TypeMap || mt == MT_DELETE {
-			value, err = columnSchema.UnmarshalSet(value)
+	if columnSchema.Type == libovsdb.TypeSet && isSetScalarOperation(columnSchema.TypeObj.Key.Type, mt) {
+		value, err = columnSchema.TypeObj.Key.Unmarshal(value)
+	} else {
+		value, err = columnSchema.Unmarshal(value)
+		if err != nil {
+			// value for mutate map with delete mutator can be map or set
+			if columnSchema.Type == libovsdb.TypeMap || mt == MT_DELETE {
+				value, err = columnSchema.UnmarshalSet(value)
+			}
 		}
 	}
+
 	if err != nil {
 		err = errors.New(E_CONSTRAINT_VIOLATION)
 		log.Error(err, "failed unmarshal of column", "column", column)
@@ -73,23 +78,23 @@ func NewMutation(tableSchema *libovsdb.TableSchema, mapUUID namedUUIDResolver, m
 	value, err = mapUUID.Resolve(value, log)
 	if err != nil {
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		log.Error(err, "failed resolv-namedUUID of column", "column", column)
+		log.Error(err, "failed resolve namedUUID of column", "column", column)
 		return nil, err
 	}
 
-	err = columnSchema.Validate(value)
+	/*err = columnSchema.Validate(value)
 	if err != nil {
 		err = errors.New(E_CONSTRAINT_VIOLATION)
 		log.Error(err, "failed validate of column", "column", column)
 		return nil, err
-	}
+	}*/
 
 	m := &Mutation{
-		Column:       column,
-		Mutator:      mt,
-		Value:        value,
-		ColumnSchema: columnSchema,
-		Log:          log,
+		column:       column,
+		mutator:      mt,
+		value:        value,
+		columnSchema: columnSchema,
+		log:          log,
 	}
 	if err = m.validateMutator(); err != nil {
 		return nil, err
@@ -98,115 +103,64 @@ func NewMutation(tableSchema *libovsdb.TableSchema, mapUUID namedUUIDResolver, m
 }
 
 func (m *Mutation) validateMutator() error {
-	switch m.ColumnSchema.Type {
+	switch m.columnSchema.Type {
 	case libovsdb.TypeReal, libovsdb.TypeInteger:
-		switch m.Mutator {
+		switch m.mutator {
 		case MT_DIFFERENCE, MT_PRODUCT, MT_QUOTIENT, MT_SUM:
 			return nil
 		case MT_REMAINDER:
-			if m.ColumnSchema.Type == libovsdb.TypeInteger {
+			if m.columnSchema.Type == libovsdb.TypeInteger {
 				return nil
 			}
 		}
-	case libovsdb.TypeMap, libovsdb.TypeSet:
-		if m.Mutator == MT_INSERT || m.Mutator == MT_DELETE {
+	case libovsdb.TypeMap:
+		if m.mutator == MT_INSERT || m.mutator == MT_DELETE {
 			return nil
 		}
-		if m.ColumnSchema.Type == libovsdb.TypeSet {
-			valueType := m.ColumnSchema.TypeObj.Value.Type
-			if valueType == libovsdb.TypeReal || valueType == libovsdb.TypeInteger {
-				switch m.Mutator {
-				case MT_DIFFERENCE, MT_PRODUCT, MT_QUOTIENT, MT_SUM:
-					return nil
-				case MT_REMAINDER:
-					if m.ColumnSchema.Type == libovsdb.TypeInteger {
-						return nil
-					}
-				}
-			}
-			err := errors.New(E_CONSTRAINT_VIOLATION)
-			m.Log.Error(err, "incompatible mutator", "mutator", m.Mutator, "set value type", valueType)
-			return err
+	case libovsdb.TypeSet:
+		if m.mutator == MT_INSERT || m.mutator == MT_DELETE {
+			return nil
+		}
+		if isSetScalarOperation(m.columnSchema.TypeObj.Key.Type, m.mutator) {
+			return nil
 		}
 	}
 	err := errors.New(E_CONSTRAINT_VIOLATION)
-	m.Log.Error(err, "incompatible mutator", "mutator", m.Mutator, "column type", m.ColumnSchema.Type)
+	m.log.Error(err, "incompatible mutator", "mutator", m.mutator, "column type", m.columnSchema.Type)
 	return err
 }
 
-func (m *Mutation) MutateInteger(row *map[string]interface{}) error {
+func (m *Mutation) mutateInteger(row *map[string]interface{}) error {
 	var err error
-	original := (*row)[m.Column].(int)
-	value, ok := m.Value.(int)
+	original := (*row)[m.column].(int)
+	value, ok := m.value.(int)
 	if !ok {
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "can't convert mutation value", "value", m.Value)
+		m.log.Error(err, "can't convert mutation value", "value", m.value)
 		return err
 	}
-	mutated := original
-	switch m.Mutator {
-	case MT_SUM:
-		mutated += value
-	case MT_DIFFERENCE:
-		mutated -= value
-	case MT_PRODUCT:
-		mutated *= value
-	case MT_QUOTIENT:
-		if value != 0 {
-			mutated /= value
-		} else {
-			err = errors.New(E_DOMAIN_ERROR)
-			m.Log.Error(err, "can't devide by 0")
-			return err
-		}
-	case MT_REMAINDER:
-		if value != 0 {
-			mutated %= value
-		} else {
-			err = errors.New(E_DOMAIN_ERROR)
-			m.Log.Error(err, "can't modulo by 0")
-			return err
-		}
-	default:
-		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "unsupported mutator", "mutator", m.Mutator)
+	mutated, err := m.mutateI(original, value)
+	if err != nil {
 		return err
 	}
-	(*row)[m.Column] = mutated
+	(*row)[m.column] = mutated
 	return nil
 }
 
-func (m *Mutation) MutateReal(row *map[string]interface{}) error {
+func (m *Mutation) mutateReal(row *map[string]interface{}) error {
 	var err error
-	original := (*row)[m.Column].(float64)
-	value, ok := m.Value.(float64)
+	original := (*row)[m.column].(float64)
+	value, ok := m.value.(float64)
 	if !ok {
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "failed to convert mutation value", "value", m.Value)
+		m.log.Error(err, "failed to convert mutation value", "value", m.value)
 		return err
 	}
-	mutated := original
-	switch m.Mutator {
-	case MT_SUM:
-		mutated += value
-	case MT_DIFFERENCE:
-		mutated -= value
-	case MT_PRODUCT:
-		mutated *= value
-	case MT_QUOTIENT:
-		if value != 0 {
-			mutated /= value
-		} else {
-			err = errors.New(E_DOMAIN_ERROR)
-			m.Log.Error(err, "can't devide by 0")
-			return err
-		}
-	default:
-		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "unsupported mutator", "mutator", m.Mutator)
+	mutated, err := m.mutateR(original, value)
+	if err != nil {
 		return err
 	}
-	(*row)[m.Column] = mutated
+	(*row)[m.column] = mutated
 	return nil
 }
 
@@ -219,16 +173,21 @@ func inSet(set *libovsdb.OvsSet, a interface{}) bool {
 	return false
 }
 
-func (m *Mutation) insertToSet(original *libovsdb.OvsSet, toInsert interface{}) (*libovsdb.OvsSet, error) {
+func (m *Mutation) insertToSet(original *libovsdb.OvsSet) (*libovsdb.OvsSet, error) {
 	var err error
-	toInsertSet, ok := toInsert.(libovsdb.OvsSet)
+	toInsertSet, ok := m.value.(libovsdb.OvsSet)
 	if !ok {
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "failed to convert mutation value", "value", toInsert)
+		m.log.Error(err, "failed to convert mutation value", "value", m.value)
 		return nil, err
 	}
 	mutated := new(libovsdb.OvsSet)
-	copier.Copy(mutated, original)
+	err = copier.Copy(mutated, original)
+	if err != nil {
+		m.log.Error(err, "copier failed")
+		err = errors.New(E_CONSTRAINT_VIOLATION)
+		return nil, err
+	}
 	for _, v := range toInsertSet.GoSet {
 		if !inSet(original, v) {
 			mutated.GoSet = append(mutated.GoSet, v)
@@ -237,12 +196,64 @@ func (m *Mutation) insertToSet(original *libovsdb.OvsSet, toInsert interface{}) 
 	return mutated, nil
 }
 
-func (m *Mutation) deleteFromSet(original *libovsdb.OvsSet, toDelete interface{}) (*libovsdb.OvsSet, error) {
+func (m *Mutation) mutateIntegersSet(original *libovsdb.OvsSet) (*libovsdb.OvsSet, error) {
 	var err error
-	toDeleteSet, ok := toDelete.(libovsdb.OvsSet)
+	value, ok := m.value.(int)
 	if !ok {
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "failed to convert mutation value", "value", toDelete)
+		m.log.Error(err, "can't convert mutation value", "value", m.value)
+		return nil, err
+	}
+
+	mutated := new(libovsdb.OvsSet)
+	for _, v := range original.GoSet {
+		intV, ok := v.(int)
+		if !ok {
+			err = errors.New(E_CONSTRAINT_VIOLATION)
+			m.log.Error(err, "can't convert mutated to int", "mutated", v)
+			return nil, err
+		}
+		newV, err := m.mutateI(intV, value)
+		if err != nil {
+			return nil, err
+		}
+		mutated.GoSet = append(mutated.GoSet, newV)
+	}
+	return mutated, nil
+}
+
+func (m *Mutation) mutateRealsSet(original *libovsdb.OvsSet) (*libovsdb.OvsSet, error) {
+	var err error
+	value, ok := m.value.(float64)
+	if !ok {
+		err = errors.New(E_CONSTRAINT_VIOLATION)
+		m.log.Error(err, "can't convert mutation value", "value", m.value)
+		return nil, err
+	}
+
+	mutated := new(libovsdb.OvsSet)
+	for _, v := range original.GoSet {
+		r, ok := v.(float64)
+		if !ok {
+			err = errors.New(E_CONSTRAINT_VIOLATION)
+			m.log.Error(err, "can't convert mutated to float64", "mutated", v)
+			return nil, err
+		}
+		newV, err := m.mutateR(r, value)
+		if err != nil {
+			return nil, err
+		}
+		mutated.GoSet = append(mutated.GoSet, newV)
+	}
+	return mutated, nil
+}
+
+func (m *Mutation) deleteFromSet(original *libovsdb.OvsSet) (*libovsdb.OvsSet, error) {
+	var err error
+	toDeleteSet, ok := m.value.(libovsdb.OvsSet)
+	if !ok {
+		err = errors.New(E_CONSTRAINT_VIOLATION)
+		m.log.Error(err, "failed to convert mutation value", "value", m.value)
 		return nil, err
 	}
 	mutated := new(libovsdb.OvsSet)
@@ -261,29 +272,44 @@ func (m *Mutation) deleteFromSet(original *libovsdb.OvsSet, toDelete interface{}
 	return mutated, nil
 }
 
-func (m *Mutation) MutateSet(row *map[string]interface{}) error {
-	original := (*row)[m.Column].(libovsdb.OvsSet)
+func (m *Mutation) mutateSet(row *map[string]interface{}) error {
+	original := (*row)[m.column].(libovsdb.OvsSet)
 	var mutated *libovsdb.OvsSet
 	var err error
-	switch m.Mutator {
+	switch m.mutator {
 	case MT_INSERT:
-		mutated, err = m.insertToSet(&original, m.Value)
+		mutated, err = m.insertToSet(&original)
 	case MT_DELETE:
-		mutated, err = m.deleteFromSet(&original, m.Value)
+		mutated, err = m.deleteFromSet(&original)
+	case MT_PRODUCT, MT_REMAINDER, MT_SUM, MT_QUOTIENT, MT_DIFFERENCE:
+		valueType := m.columnSchema.TypeObj.Key.Type
+		if valueType == libovsdb.TypeInteger {
+			mutated, err = m.mutateIntegersSet(&original)
+		} else if valueType == libovsdb.TypeReal {
+			mutated, err = m.mutateRealsSet(&original)
+		} else {
+			err = errors.New(E_CONSTRAINT_VIOLATION)
+			m.log.Error(err, "incompatible mutator and set's value type:", "mutator", m.mutator, "value type", valueType)
+		}
 	default:
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "unsupported mutation mutator:", "mutator", m.Mutator)
+		m.log.Error(err, "unsupported mutation mutator:", "mutator", m.mutator)
 	}
 	if err != nil {
 		return err
 	}
-	(*row)[m.Column] = *mutated
+	(*row)[m.column] = *mutated
 	return nil
 }
 
 func (m *Mutation) insertToMap(original *libovsdb.OvsMap, toInsert interface{}) (*libovsdb.OvsMap, error) {
 	mutated := new(libovsdb.OvsMap)
-	copier.Copy(&mutated, &original)
+	err := copier.Copy(&mutated, &original)
+	if err != nil {
+		m.log.Error(err, "copier failed")
+		err = errors.New(E_CONSTRAINT_VIOLATION)
+		return nil, err
+	}
 	switch toInsert := toInsert.(type) {
 	case libovsdb.OvsMap:
 		for k, v := range toInsert.GoMap {
@@ -293,7 +319,7 @@ func (m *Mutation) insertToMap(original *libovsdb.OvsMap, toInsert interface{}) 
 		}
 	default:
 		err := errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "unsupported mutator value type", "value", toInsert)
+		m.log.Error(err, "unsupported mutator value type", "value", toInsert)
 		return nil, err
 	}
 	return mutated, nil
@@ -301,7 +327,12 @@ func (m *Mutation) insertToMap(original *libovsdb.OvsMap, toInsert interface{}) 
 
 func (m *Mutation) deleteFromMap(original *libovsdb.OvsMap, toDelete interface{}) (*libovsdb.OvsMap, error) {
 	mutated := new(libovsdb.OvsMap)
-	copier.Copy(&mutated, &original)
+	err := copier.Copy(&mutated, &original)
+	if err != nil {
+		m.log.Error(err, "copier failed")
+		err = errors.New(E_CONSTRAINT_VIOLATION)
+		return nil, err
+	}
 	switch toDelete := toDelete.(type) {
 	case libovsdb.OvsMap:
 		for k, v := range toDelete.GoMap {
@@ -317,52 +348,129 @@ func (m *Mutation) deleteFromMap(original *libovsdb.OvsMap, toDelete interface{}
 	return mutated, nil
 }
 
-func (m *Mutation) MutateMap(row *map[string]interface{}) error {
-	original := (*row)[m.Column].(libovsdb.OvsMap)
+func (m *Mutation) mutateMap(row *map[string]interface{}) error {
+	original := (*row)[m.column].(libovsdb.OvsMap)
 	mutated := new(libovsdb.OvsMap)
 	var err error
-	switch m.Mutator {
+	switch m.mutator {
 	case MT_INSERT:
-		mutated, err = m.insertToMap(&original, m.Value)
+		mutated, err = m.insertToMap(&original, m.value)
 	case MT_DELETE:
-		mutated, err = m.deleteFromMap(&original, m.Value)
+		mutated, err = m.deleteFromMap(&original, m.value)
 	default:
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "unsupported mutation mutator", "mutator", m.Mutator)
+		m.log.Error(err, "unsupported mutation mutator", "mutator", m.mutator)
 		return err
 	}
 	if err != nil {
 		return err
 	}
-	(*row)[m.Column] = *mutated
+	(*row)[m.column] = *mutated
 	return nil
 }
 
 func (m *Mutation) Mutate(row *map[string]interface{}) error {
 	var err error
-	switch m.Column {
+	switch m.column {
 	case libovsdb.COL_UUID, libovsdb.COL_VERSION:
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "can't mutate column", "column", m.Column)
+		m.log.Error(err, "can't mutate column", "column", m.column)
 		return err
 	}
-	if m.ColumnSchema.Mutable != nil && !*m.ColumnSchema.Mutable {
+	if m.columnSchema.Mutable != nil && !*m.columnSchema.Mutable {
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "can't mutate unmutable column", "column", m.Column)
+		m.log.Error(err, "can't mutate immutable column", "column", m.column)
 		return err
 	}
-	switch m.ColumnSchema.Type {
+	switch m.columnSchema.Type {
 	case libovsdb.TypeInteger:
-		return m.MutateInteger(row)
+		return m.mutateInteger(row)
 	case libovsdb.TypeReal:
-		return m.MutateReal(row)
+		return m.mutateReal(row)
 	case libovsdb.TypeSet:
-		return m.MutateSet(row)
+		return m.mutateSet(row)
 	case libovsdb.TypeMap:
-		return m.MutateMap(row)
+		return m.mutateMap(row)
 	default:
 		err = errors.New(E_CONSTRAINT_VIOLATION)
-		m.Log.Error(err, "unsupported column schema type", "type", m.ColumnSchema.Type)
+		m.log.Error(err, "unsupported column schema type", "type", m.columnSchema.Type)
 		return err
+	}
+}
+
+func (m *Mutation) mutateI(mutated, value int) (int, error) {
+	switch m.mutator {
+	case MT_SUM:
+		mutated += value
+	case MT_DIFFERENCE:
+		mutated -= value
+	case MT_PRODUCT:
+		mutated *= value
+	case MT_QUOTIENT:
+		if value != 0 {
+			mutated /= value
+		} else {
+			err := errors.New(E_DOMAIN_ERROR)
+			m.log.Error(err, "can't divide by 0")
+			return -1, err
+		}
+	case MT_REMAINDER:
+		if value != 0 {
+			mutated %= value
+		} else {
+			err := errors.New(E_DOMAIN_ERROR)
+			m.log.Error(err, "can't modulo by 0")
+			return -1, err
+		}
+	default:
+		err := errors.New(E_CONSTRAINT_VIOLATION)
+		m.log.Error(err, "unsupported mutator", "mutator", m.mutator)
+		return -1, err
+	}
+	return mutated, nil
+}
+
+func (m *Mutation) mutateR(mutated, value float64) (float64, error) {
+	switch m.mutator {
+	case MT_SUM:
+		mutated += value
+	case MT_DIFFERENCE:
+		mutated -= value
+	case MT_PRODUCT:
+		mutated *= value
+	case MT_QUOTIENT:
+		if value != 0 {
+			mutated /= value
+		} else {
+			err := errors.New(E_DOMAIN_ERROR)
+			m.log.Error(err, "can't divide by 0")
+			return -1, err
+		}
+	default:
+		err := errors.New(E_CONSTRAINT_VIOLATION)
+		m.log.Error(err, "unsupported mutator", "mutator", m.mutator)
+		return -1, err
+	}
+	return mutated, nil
+}
+
+func isSetScalarOperation(setValuesType, mutator string) bool {
+	switch setValuesType {
+	case libovsdb.TypeInteger:
+		switch mutator {
+		case MT_DIFFERENCE, MT_PRODUCT, MT_QUOTIENT, MT_SUM, MT_REMAINDER:
+			return true
+		default:
+			return false
+		}
+	case libovsdb.TypeReal:
+		switch mutator {
+		case MT_DIFFERENCE, MT_PRODUCT, MT_QUOTIENT, MT_SUM:
+			return true
+		default:
+			return false
+		}
+	default:
+		return false
 	}
 }
